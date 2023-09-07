1dbeed1e8fd0aa8c946ee85c1da618ffbd884200
Elias Godager <eliashgod@gmail.com>
2023-09-07 11:29:21 +0200

    ferdig med confetti og ekstra 0

--- Grunnleggende_Programmering/intro/src/App.js
+++ Grunnleggende_Programmering/intro/src/App.js
@@ -4,7 +4,7 @@ import Oppgave1 from './oppgave1';
 import Mattematikk from './oppgave2';
 import Oppgave3 from './oppgave3';
 import NumberGuesser from './oppgave4';
-import DigitalClock from './digitalClock';
+
 
 
 function App() {
@@ -16,7 +16,6 @@ function App() {
       {/* <TallGjett /> */}
       {/* <Oppgave3 /> */}
       {/* <NumberGuesser /> */}
-      <DigitalClock />
       </header>
     </div>
   );
--- React_intro/my-app/src/DigitalClock.js
+++ React_intro/my-app/src/DigitalClock.js
@@ -1,33 +1,51 @@
 import { useState, useEffect} from 'react';
+import ConfettiExplosion from 'react-confetti-explosion';
 
 export default function DigitalClock() {
 
+    let confetti = '' 
     let clock = new Date();
     let hh = clock.getHours();
     let mm = clock.getMinutes();
     let ss = clock.getSeconds();
-    let time = [hh+':' +mm+':' +ss];
     const[currentTime, newTime] = useState(new Date);
 
     console.log(clock)
 
+    if (ss < 10) {
+        ss = '0'+clock.getSeconds();
+    };
+    if (mm < 10) {
+        mm = '0'+clock.getMinutes();
+    }
+    if (hh < 10) {
+        hh = '0'+clock.getHours()
+    }
+
+    if (currentTime.getSeconds()===0) {
+         confetti = <ConfettiExplosion />
+    } else {
+        confetti = null
+    };
+
+    let time = [hh+':' +mm+':' +ss];
+    
     useEffect(() => {
 
         const myInterval = setInterval(() => {
         newTime(new Date)
-        }, 1000);
+        }, 500);
 
         return () => clearInterval(myInterval)
 
     }, []);
-
 
     return(
         <div className='App'>
             <header className='App-header'>
                 
             <h1>{time}</h1>
-
+                {confetti}
             </header>
         </div>
     )
