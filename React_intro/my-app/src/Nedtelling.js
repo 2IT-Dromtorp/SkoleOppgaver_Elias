5af182541f9a16e0a27ff96dbee54d9003eceb37
Elias Godager <eliashgod@gmail.com>
2023-09-14 11:16:29 +0200

    må committe

--- React_intro/my-app/src/Nedtelling.js
+++ React_intro/my-app/src/Nedtelling.js
@@ -1,28 +1,46 @@
-import {useState, useEffect, ChangeEvent} from 'react';
+import React, { useState, useEffect } from 'react';
 import ConfettiExplosion from 'react-confetti-explosion';
-import './App.css'
+import './App.css';
 
 export default function NedTelling() {
-    
-    let confetti = ''
-    let inputText = ''
-    let setInputText = ''
+  const [endreTid, setEndreTid] = useState(10);
+  let confetti = '';
+  let myInterval;
 
+  const handleChange = (e) => {
+    setEndreTid(parseInt(e.target.value, 10));
+  };
 
-    let endreTid = [inputText, setInputText] = useState('');
-
-    const handleChange = (e) => {
-        setInputText(e.target.value);
+  useEffect(() => {
+    if (endreTid > 0) {
+      myInterval = setInterval(() => {
+        setEndreTid((prevTid) => prevTid - 1); 
+      }, 1000);
     }
-    
-    if (endreTid != Number) { 
-        return(null)
+
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, [endreTid]);
+
+  useEffect(() => {
+    if (endreTid === 0) {
+      clearInterval(myInterval);
     }
-    return(
-        <div className='App'>
-            <header className='App-header'>
-                <input type="text" onChange={handleChange} value={inputText} />
-            </header>
-        </div>
-    )
+  }, [endreTid]);
+
+  if (endreTid === 0) {
+    confetti = <ConfettiExplosion />;
+  } 
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        {endreTid}
+        {confetti}
+        <br></br>
+        <input type="number" onChange={handleChange} value={endreTid} />
+      </header>
+    </div>
+  );
 }
