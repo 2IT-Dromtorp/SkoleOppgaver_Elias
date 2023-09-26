0ed0fd34976d0054c8e0abaf76524c6b1842fe5f
Elias Godager <eliashgod@gmail.com>
2023-09-26 13:02:56 +0200

    我要推翻蒙古政府

--- todo/src/TimeNDate.js
+++ todo/src/TimeNDate.js
@@ -1,5 +1,4 @@
 
-
 export default function TnD (){
     let TnD = new Date();
     let date = TnD.getDate();
@@ -7,8 +6,20 @@ export default function TnD (){
     let weekDay = TnD.getDay();
     let weekDayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     let monthNames = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let crythst = ['test', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'st'];
+    //jeg er lat... og det fungerer.
 
-    console.log(date)
-    console.log(monthNames[month])
-    
+    let displayMonth = monthNames[month];
+    let displayWeek = weekDayNames[weekDay];
+    let displayth = crythst[date];
+
+    console.log(crythst.length);
+    console.log(date);
+    console.log(monthNames[month]);
+    console.log(weekDayNames[weekDay]);
+     
+
+    return(
+        <h1>{displayWeek}<br></br>{date}{displayth}<br></br>{displayMonth}</h1>
+    )
 }
